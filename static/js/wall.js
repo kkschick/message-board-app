a43848aaeb05f54d9cb327056e46c922327f7b52
Joel Burton <joel@joelburton.com>
2014-10-18 12:18:08 -0700

    Clean up JS for minor idioms, and Crockford style.
    Also: added comments to a few places where things might be unobvious
    to them--particularly the use of "var self = this", to allow a closure
    in the setTimeout() function.
    
    Changed the signature of showTempResultMessage() to take the
    msg-to-be-set into this and actually manipulate the DOM here, rather
    than in init. This seemed like this would be a useful refactoring for
    the additional work they have to do afterwards.

--- static/js/wall.js
+++ static/js/wall.js
@@ -1,24 +1,21 @@
-$(document).ready(function()
-{
-	init();
+$(document).ready(function () {
+    // Normally, JavaScript runs code at the time that the <script>
+    // tags loads the JS. By putting this inside a jQuery $(document).ready()
+    // function, this code only gets run when the document finishing loading.
+	initWallApplication();
 });
 
 /**
  * The init function is run once the document is
  * completely loaded (and drunk).
  */
-function init()
-{
-	$("#message-form").submit(function(e)
-	{
+function initWallApplication() {
+	$("#message-form").submit(function (e) {
 		e.preventDefault();
 		
 		console.log("Submitting new message.");
-
 		var url = "/api/wall/set";
-		var self = this;
-
-		sendMessageDataToServer(self);
+    	sendMessageDataToServer(this, url);
 
 		// Reset the message container to be empty
 		$("#message").val("");
@@ -29,36 +26,45 @@ function init()
  * This function makes the AJAX call to the server
  * and passes the message to it.
  */
-function sendMessageDataToServer(formObj)
-{
+function sendMessageDataToServer(formObj, url) {
 	// Send the message data to the server to be stored
 	$.ajax({
 		dataType: "json",
 		url: url,
 		type: 'POST',
 		data: $(formObj).serialize(),
-		success: function(data)
-		{
-			console.log(data);
-			$("#sent-result").text(data.result);
-
-			showTempResultMessage();
+		success: function (data) {
+			console.log("sendMessageDataToServer: ", data);
+			showTempResultMessage(data.result);
 		}
 	});
 }
 
 /*
- * This is a helper function that does nothing but
- * show a section of the site (the message result)
- * and then hide it a moment later.
+ * This is a helper function that does nothing but show a section of the
+ * site (the message result) and then hide it a moment later.
  */
-function showTempResultMessage()
-{
-	$("#sent-result").slideDown(function()
-	{
+function showTempResultMessage(resultMsg) {
+    var notificationArea = $("#sent-result");
+    notificationArea.text(resultMsg);
+    notificationArea.slideDown(function () {
+        // In JavaScript, "this" is a keyword that means "the object this
+        // method or function is called on"; it is analogous to Python's
+        // "self". In our case, "this" is the #sent-results element, which
+        // is what slideDown was called on.
+        //
+        // However, when setTimeout is called, it won't be called on that
+        // same #sent-results element--"this", for it, wouldn't be that
+        // element. We could put inside of our setTimeout call the code
+        // to re-find the #sent-results element, but that would be a bit
+        // inelegant. Instead, we'll use a common JS idiom, to set a variable
+        // to the *current* definition of self, here in this outer function,
+        // so that the inner function can find it and where it will have the
+        // same value. When stashing "this" into a new variable like that,
+        // many JS programmers use the name "self"; some others use "that".
 		var self = this;
-		setTimeout(function()
-		{
+
+		setTimeout(function () {
 			$(self).slideUp();
 		}, 2000);
 	});
